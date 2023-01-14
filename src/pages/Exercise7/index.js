b4f5f59ffcbec8aa6ad62899fd731efff854896a
Lorenzo Covarrubias <lorenzo.covarrubias@slalom.com>
2023-01-14 14:25:27 -0600

    Added solutions to optimize

--- src/pages/Exercise7/index.js
+++ src/pages/Exercise7/index.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Loader } from "./Loader";
 
-import './styles.css';
+import "./styles.css";
 
 const Details = React.lazy(() => {
   // Artificially making loadind the <Details> component take 1 second
   return new Promise((resolve, reject) => {
-    return setTimeout(() => resolve(import('./Details')), 1000);
+    return setTimeout(() => resolve(import("./Details")), 1000);
   });
 });
 
 export const Exercise7 = () => {
   return (
-    <div className='container'>
-      <Link to='/'>← Back to Home</Link>
+    <div className="container">
+      <Link to="/">← Back to Home</Link>
       <h1>Exercise 7: Suspense</h1>
-      <div className='e7-container'>
-        <div className='e7-content-container'>
-          <img src={require('./logo.png')} alt='Restaurant logo' />
+      <div className="e7-container">
+        <div className="e7-content-container">
+          <img src={require("./logo.png")} alt="Restaurant logo" />
           <h1>Your reservation is confirmed.</h1>
         </div>
-        <Details />
+        <Suspense fallback={<Loader />}>
+          <Details />
+        </Suspense>
       </div>
     </div>
   );
