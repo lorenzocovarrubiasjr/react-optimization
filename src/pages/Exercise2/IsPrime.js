688a41bde0364cebdb8534ceac385e800fcf4a72
Lorenzo Covarrubias <lorenzo.covarrubias@slalom.com>
2023-01-14 13:14:10 -0600

    first commit, optimization exercises loaded

--- src/pages/Exercise2/IsPrime.js
+++ src/pages/Exercise2/IsPrime.js
@@ -1,16 +1,17 @@
-import { checkIfPrime } from './helpers';
+import { useMemo } from "react";
+import { checkIfPrime } from "./helpers";
 
 export const IsPrime = (props) => {
   const { number, showExplainer } = props;
-  const isPrime = checkIfPrime(number);
+  const isPrime = useMemo(() => checkIfPrime(number), [number]);
 
   return (
     <div>
-      <h3 className='e2-result'>
-        {isPrime ? 'Confirmed prime number' : 'Not a prime number'}
+      <h3 className="e2-result">
+        {isPrime ? "Confirmed prime number" : "Not a prime number"}
       </h3>
       {showExplainer && (
-        <p className='e2-explainer'>
+        <p className="e2-explainer">
           Prime numbers are a positive integer that is greater than 1 and is not
           divisible without a remainder by any positive integer other than
           itself and 1.
